3d6ab2c36ac336f49e3e2f9cde7d42e18bf8c778
PowerChaos <PowerChaos@users.noreply.github.com>
2016-09-16 07:49:27 +0200

    use as homepage only

--- functions/include.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once (getenv("DOCUMENT_ROOT")."/functions/database.php");
-require_once (getenv("DOCUMENT_ROOT")."/functions/sessions.php");
-require_once (getenv("DOCUMENT_ROOT")."/functions/pages.php");
-require_once (getenv("DOCUMENT_ROOT")."/functions/permissions.php");
-require_once (getenv("DOCUMENT_ROOT")."/functions/PasswordStorage.php");
-/* CopyRight PowerChaos 2016 */
-?>
